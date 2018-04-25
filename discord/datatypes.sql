310eebd6b8c721630135e6b43a8b57ce9aa9c0ab
Colton Loewen <loewencjay@gmail.com>
2018-04-24 21:18:58 -0600

    Removed duplicate checks

--- discord/datatypes.sql
+++ discord/datatypes.sql
@@ -3,11 +3,7 @@
 allow - Only allow the types specified.
 deny - Do not allow the types specified.
 */
-DO $$ BEGIN
-	CREATE TYPE discord.accessType AS ENUM ('allow', 'deny');
-EXCEPTION
-	WHEN duplicate_object THEN null;
-END $$;
+CREATE TYPE discord.accessType AS ENUM ('allow', 'deny');
 /*
 everyone - Applies the method to everyone.
 role - Checks the users roles.
@@ -15,8 +11,4 @@ channel - Checks the channel that the command was used in.
 server - Checks the server that the command was used in.
 dm - Checks if the command is being called from a direct message.
 */
-DO $$ BEGIN
-	CREATE TYPE discord.accessMethod AS ENUM ('dm', 'server', 'everyone', 'channel', 'role');
-EXCEPTION
-	WHEN duplicate_object THEN null;
-END $$;
+CREATE TYPE discord.accessMethod AS ENUM ('dm', 'server', 'everyone', 'channel', 'role');
