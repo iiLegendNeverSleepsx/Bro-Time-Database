3b9240a0f76b20135954bfc3a909c9fd849004cf
Colton Loewen <loewencjay@gmail.com>
2018-04-28 18:34:30 -0600

    Used variables with pg_prove

--- install.sh
+++ install.sh
@@ -11,14 +11,14 @@ fi;
 
 # Load settings:
 . install.cfg
-read -p "Please input database schema owner ($db_user) password:" db_pass
+read -p "The database schema owner is $db_user, password:" db_pass
 
 echo "Creating database."
 psql -v db_user=$db_user -v db_pass="'$db_pass'" -v db_name=$db_name -h $db_host -U postgres -f create_db.sql &>install.log
 
-echo "Enabling extensions in database. Enter your postgres user password."
+echo "Enabling extensions in database."
 psql -h $db_host -d $db_name -U postgres -f extensions.sql &>>install.log
 
-echo "Creating schema. Enter your schema user password."
+echo "Creating schema."
 psql -h $db_host -d $db_name -U $db_user -f install.sql &>> install.log
 
