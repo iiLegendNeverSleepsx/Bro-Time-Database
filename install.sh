b5634786934c22fa4ad109efe031aee1cf5b2086
Colton Loewen <loewencjay@gmail.com>
2018-04-28 22:44:40 -0600

    Merged variables

--- install.sh
+++ install.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 set -e
 
-if [ ! -f install.cfg ] ; then
- echo 'Error! File install.cfg does not exist. Please create it:
-	$ cp install.cfg.example install.cfg
-	$ vi install.cfg'
-
- exit 1
-fi;
-
-# Load settings:
-. install.cfg
-read -p "The database schema owner is $db_user, password:" db_pass
-
 echo "Creating database."
-psql -p $db_port -v db_user=$db_user -v db_pass="'$db_pass'" -v db_name=$db_name -h $db_host -U postgres -f create_db.sql &>install.log
+psql -v db_user=$db_user -v db_pass="'$db_pass'" -v db_name=$db_name -h $db_host -U postgres -f create_db.sql &>install.log
 
 echo "Enabling extensions in database."
-psql -h $db_host -p $db_port -d $db_name -U postgres -f extensions.sql &>>install.log
+psql -h $db_host -d $db_name -U postgres -f extensions.sql &>>install.log
 
 echo "Creating schema."
-psql -h $db_host -p $db_port -d $db_name -U $db_user -f install.sql &>> install.log
+psql -h $db_host -d $db_name -U $db_user -f install.sql &>> install.log
 
