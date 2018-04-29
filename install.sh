6ef33461812b23db247c7068fd1c3e178b37fcce
Colton Loewen <loewencjay@gmail.com>
2018-04-28 18:44:55 -0600

    Added port removed variable

--- install.sh
+++ install.sh
@@ -14,11 +14,11 @@ fi;
 read -p "The database schema owner is $db_user, password:" db_pass
 
 echo "Creating database."
-psql -v db_user=$db_user -v db_pass="'$db_pass'" -v db_name=$db_name -h $db_host -U postgres -f create_db.sql &>install.log
+psql -p $db_port -v db_user=$db_user -v db_pass="'$db_pass'" -v db_name=$db_name -h $db_host -U postgres -f create_db.sql &>install.log
 
 echo "Enabling extensions in database."
-psql -h $db_host -d $db_name -U postgres -f extensions.sql &>>install.log
+psql -h $db_host -p $db_port -d $db_name -U postgres -f extensions.sql &>>install.log
 
 echo "Creating schema."
-psql -h $db_host -d $db_name -U $db_user -f install.sql &>> install.log
+psql -h $db_host -p $db_port -d $db_name -U $db_user -f install.sql &>> install.log
 
