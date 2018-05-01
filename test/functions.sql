b9a888c8d863cb2238df42bb55d4732224140630
Colton Loewen <loewencjay@gmail.com>
2018-05-01 03:29:51 -0600

    Added column tests

--- test/functions.sql
+++ test/functions.sql
@@ -10,7 +10,28 @@ FROM information_schema.columns
 WHERE table_schema = 'discord' AND data_type IN ('ARRAY', 'json', 'jsonb', 'xml');
 
 CREATE FUNCTION test.columns() RETURNS setof text AS $$
+DECLARE
+	column record;
+	all cursor FOR SELECT schema, table, column
+		FROM test.columns;
+	unlimited cursor FOR SELECT schema, table, column
+		FROM test.columns_unlimited;
 BEGIN   
-	
+	OPEN all;
+	LOOP
+		FETCH all INTO column;
+		EXIT WHEN NOT FOUND;
+
+		RETURN NEXT matches(column.column, '^(?:[a-z]+_)+[a-z]+$', 'Column name contains alphabetic words separated by underscores.');
+	END LOOP;
+	CLOSE all;
+	OPEN unlimited;
+	LOOP
+		FETCH unlimited INTO column;
+		EXIT WHEN NOT FOUND;
+
+		RETURN NEXT col_has_check(column.schema, column.table, column.column, 'Columns that are not implicitly limited on range must be explicitly limited.');
+	END LOOP;
+	CLOSE unlimited;
 END;
 $$ LANGUAGE plpgsql;
