6357197492ebc29d5e86150f3f244e320c2d51ac
Colton Loewen <loewencjay@gmail.com>
2018-05-02 00:47:38 -0600

    Added tests alias

--- test/tests/run_discord.sql
+++ test/tests/run_discord.sql
@@ -2,7 +2,7 @@
 SELECT plan(total)
 FROM (SELECT CAST(SUM(total) AS integer)
 	FROM test.tests
-	WHERE schema = 'shared' OR schema = 'discord');
+	WHERE schema = 'shared' OR schema = 'discord') AS tests;
 
 -- Standard Tests
 SELECT * FROM test.columns();
