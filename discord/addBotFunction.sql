67797cca326018cf01902826ac0a4e7bfb141534
Colton Loewen <loewencjay@gmail.com>
2018-05-02 17:57:36 -0600

    Added existance check

--- discord/addBotFunction.sql
+++ discord/addBotFunction.sql
@@ -1,5 +1,8 @@
 CREATE OR REPLACE FUNCTION discord.AddBot(p_Server_Id bigint) RETURNS void AS $$
 BEGIN
+	IF p_Server_Id IS NOT NULL THEN
+		RAISE SQLSTATE '22004' USING DETAIL = 'p_Server_Id must be provided.';
+	END IF;
     INSERT INTO discord.Servers(Server_Id)
 		VALUES(p_Server_Id)
 	ON CONFLICT ON CONSTRAINT Servers_Server_Id_PK DO NOTHING;
