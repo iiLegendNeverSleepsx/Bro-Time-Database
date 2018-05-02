5a370b1c87d0e29ab9a08b4e8373fc555317dc1d
Colton Loewen <loewencjay@gmail.com>
2018-05-02 14:24:46 -0600

    Echoing all

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "Creating database."
-psql -v db_name=$db_name -h $db_host -p $db_port -U postgres -f create_db.sql -b &>install.log
+psql -v db_name=$db_name -h $db_host -p $db_port -U postgres -f create_db.sql -b -a &>install.log
 
 echo "Enabling extensions in database."
-psql -h $db_host -p $db_port -d $db_name -U postgres -f extensions.sql -b &>>install.log
+psql -h $db_host -p $db_port -d $db_name -U postgres -f extensions.sql -b -a &>>install.log
 
 echo "Creating schema."
-psql -h $db_host -p $db_port -d $db_name -U postgres -f install.sql -b &>> install.log
+psql -h $db_host -p $db_port -d $db_name -U postgres -f install.sql -b -a &>> install.log
