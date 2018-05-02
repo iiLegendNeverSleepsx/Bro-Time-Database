ae2de92d0d4622bd1c542da4d81ded12891f3d2a
Colton Loewen <loewencjay@gmail.com>
2018-05-02 14:16:22 -0600

    Added psql error output

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "Creating database."
-psql -v db_name=$db_name -h $db_host -p $db_port -U postgres -f create_db.sql &>install.log
+psql -v db_name=$db_name -h $db_host -p $db_port -U postgres -f create_db.sql -b &>install.log
 
 echo "Enabling extensions in database."
-psql -h $db_host -p $db_port -d $db_name -U postgres -f extensions.sql &>>install.log
+psql -h $db_host -p $db_port -d $db_name -U postgres -f extensions.sql -b &>>install.log
 
 echo "Creating schema."
-psql -h $db_host -p $db_port -d $db_name -U postgres -f install.sql &>> install.log
+psql -h $db_host -p $db_port -d $db_name -U postgres -f install.sql -b &>> install.log
