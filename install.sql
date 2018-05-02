7c733aca82eb76c2ba975ed848e3cb3e2af0961b
Colton Loewen <loewencjay@gmail.com>
2018-05-02 02:58:49 -0600

    Added commit
    Also got rid of the explicit transaction, it was a bad idea.

--- install.sql
+++ install.sql
@@ -1,10 +1,10 @@
 \set ECHO all
 \qecho 'Installing database schema...'
 
-BEGIN;
 -- Shared
 \i test/schema.sql
 \i test/functions.sql
+COMMIT;
 
 -- Schema
 \i discord/schema.sql
@@ -14,6 +14,5 @@ BEGIN;
 \i discord/tables.sql
 -- Functions
 \i discord/functions.sql
-COMMIT;
 
 \qecho 'Done.'
