11c23693ca3c80bdb5c2f5fc0ec3922190c72831
Colton Loewen <loewencjay@gmail.com>
2018-05-02 00:42:53 -0600

    Changed plan to subquery

--- test/tests/run_discord.sql
+++ test/tests/run_discord.sql
@@ -1,7 +1,8 @@
 \i setup.sql
-SELECT plan(CAST(SUM(total) AS integer))
-FROM test.tests
-WHERE schema = 'shared' OR schema = 'discord';
+SELECT plan(total)
+FROM (SELECT CAST(SUM(total) AS integer)
+	FROM test.tests
+	WHERE schema = 'shared' OR schema = 'discord');
 
 -- Standard Tests
 SELECT * FROM test.columns();
