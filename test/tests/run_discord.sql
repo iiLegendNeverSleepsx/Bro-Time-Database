c26078a4f076a93838b797a5b041d2f864312c1e
Colton Loewen <loewencjay@gmail.com>
2018-05-02 00:31:22 -0600

    Adjusted plan syntex

--- test/tests/run_discord.sql
+++ test/tests/run_discord.sql
@@ -1,7 +1,7 @@
 \i setup.sql
-SELECT plan(SELECT SUM(total)
-	FROM test.tests
-	WHERE schema = 'shared' OR schema = 'discord');
+SELECT plan(SUM(total))
+FROM test.tests
+WHERE schema = 'shared' OR schema = 'discord';
 
 -- Standard Tests
 SELECT * FROM test.columns();
