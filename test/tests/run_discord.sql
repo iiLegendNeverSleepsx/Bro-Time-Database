4a9cd717fe9950fd8530d9dfbb5abec89a147b92
Colton Loewen <loewencjay@gmail.com>
2018-05-01 21:47:23 -0600

    Attempting without rollback

--- test/tests/run_discord.sql
+++ test/tests/run_discord.sql
@@ -1,9 +1,7 @@
 \i setup.sql
-SELECT plan(CAST(currval('"test.tests"') AS integer));
+SELECT plan(CAST(currval('test.tests') AS integer));
 
 -- Standard Tests
 SELECT * FROM test.columns();
 
 SELECT * FROM finish();
-
-ROLLBACK;
