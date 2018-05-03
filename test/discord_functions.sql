b7e5bda89f5ad19a5e77586597d904536391435a
Colton Loewen <loewencjay@gmail.com>
2018-05-02 19:52:15 -0600

    Added pk test

--- test/discord_functions.sql
+++ test/discord_functions.sql
@@ -6,6 +6,7 @@ BEGIN
 	RETURN NEXT lives_ok('SELECT discord.AddBot(0) FOR UPDATE;', 'discord.AddBot must not throw if called correctly.');
 	RETURN NEXT results_eq('SELECT Server_Id FROM discord.Servers WHERE Server_Id = 0;', 'SELECT CAST(0 AS bigint) AS Server_Id;',  'discord.AddBot must have added a Servers record.');
 	-- Does not create another record if called again.
+	RETURN NEXT col_is_pk('discord', 'Servers', 'Server_Id', 'The Server_Id must uniquely identify the record.');
 	RETURN NEXT lives_ok('SELECT discord.AddBot(0) FOR UPDATE;', 'discord.AddBot must not throw if called again.');
 	RETURN NEXT results_eq('SELECT Server_Id FROM discord.Servers WHERE Server_Id = 0;', 'SELECT CAST(0 AS bigint) AS Server_Id;',  'discord.AddBot must keep existing Servers records.');
 END;
