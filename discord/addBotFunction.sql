c485bb1e41e1616e2d6034688987330eccf8cd12
Colton Loewen <loewencjay@gmail.com>
2018-05-02 18:50:52 -0600

    Added exception message

--- discord/addBotFunction.sql
+++ discord/addBotFunction.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION discord.AddBot(p_Server_Id bigint) RETURNS void AS $$
 BEGIN
 	IF p_Server_Id IS NULL THEN
-		RAISE SQLSTATE '22004' USING DETAIL = 'p_Server_Id must be provided.';
+		RAISE SQLSTATE '22004' USING MESSAGE = 'p_Server_Id must be provided.';
 	END IF;
     INSERT INTO discord.Servers(Server_Id)
 		VALUES(p_Server_Id)
--- test/discord_functions.sql
+++ test/discord_functions.sql
@@ -1,7 +1,7 @@
 CREATE FUNCTION test.AddBot() RETURNS setof text AS $$
 BEGIN
 	-- Throws if arguments are null.
-	RETURN NEXT throws_ok('SELECT discord.AddBot(null) FOR UPDATE;', '22004', 'discord.AddBot should not accept null arguments.');
+	RETURN NEXT throws_ok('SELECT discord.AddBot(null) FOR UPDATE;', '22004', 'p_Server_Id must be provided.', 'discord.AddBot should not accept null arguments.');
 	-- Creates a Servers record.
 	RETURN NEXT lives_ok('SELECT discord.AddBot(0) FOR UPDATE;', 'discord.AddBot must not throw if called correctly.');
 	RETURN NEXT results_eq('SELECT Server_Id FROM discord.Servers WHERE Server_Id = 0;', 'SELECT 0;',  'discord.AddBot must have added a Servers record.');
