fe92b23098dda08cf7d507b184cefe196d7470f0
Colton Loewen <loewencjay@gmail.com>
2018-05-02 18:46:35 -0600

    Revert "Trying reverse throws"
    This reverts commit 79d7745eb62abc6433812594a6b02986426b6323.

--- test/discord_functions.sql
+++ test/discord_functions.sql
@@ -1,12 +1,12 @@
 CREATE FUNCTION test.AddBot() RETURNS setof text AS $$
 BEGIN
 	-- Throws if arguments are null.
-	RETURN NEXT lives_ok('SELECT discord.AddBot(null) FOR UPDATE;', 'discord.AddBot should not accept null arguments.');
+	RETURN NEXT throws_ok('SELECT discord.AddBot(null) FOR UPDATE;', '22004', 'discord.AddBot should not accept null arguments.');
 	-- Creates a Servers record.
-	RETURN NEXT throws_ok('SELECT discord.AddBot(0) FOR UPDATE;', 'discord.AddBot must not throw if called correctly.');
+	RETURN NEXT lives_ok('SELECT discord.AddBot(0) FOR UPDATE;', 'discord.AddBot must not throw if called correctly.');
 	RETURN NEXT results_eq('SELECT Server_Id FROM discord.Servers WHERE Server_Id = 0;', 'SELECT 0;',  'discord.AddBot must have added a Servers record.');
 	-- Does not create another record if called again.
-	RETURN NEXT throws_ok('SELECT discord.AddBot(0) FOR UPDATE;', 'discord.AddBot must not throw if called again.');
+	RETURN NEXT lives_ok('SELECT discord.AddBot(0) FOR UPDATE;', 'discord.AddBot must not throw if called again.');
 	RETURN NEXT results_eq('SELECT Server_Id FROM discord.Servers WHERE Server_Id = 0;', 'SELECT 0;',  'discord.AddBot must keep existing Servers records.');
 END;
 $$ LANGUAGE plpgsql;
