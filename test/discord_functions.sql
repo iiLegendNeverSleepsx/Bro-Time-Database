ef8650fb048edac7c37215a5e20629307000ed65
Colton Loewen <loewencjay@gmail.com>
2018-05-02 19:45:47 -0600

    Properly casted data type

--- test/discord_functions.sql
+++ test/discord_functions.sql
@@ -4,9 +4,9 @@ BEGIN
 	RETURN NEXT throws_ok('SELECT discord.AddBot(null) FOR UPDATE;', '22004', 'p_Server_Id must be provided.', 'discord.AddBot should not accept null arguments.');
 	-- Creates a Servers record.
 	RETURN NEXT lives_ok('SELECT discord.AddBot(0) FOR UPDATE;', 'discord.AddBot must not throw if called correctly.');
-	RETURN NEXT results_eq('SELECT Server_Id FROM discord.Servers WHERE Server_Id = 0;', 'SELECT bigint 0 AS Server_Id;',  'discord.AddBot must have added a Servers record.');
+	RETURN NEXT results_eq('SELECT Server_Id FROM discord.Servers WHERE Server_Id = 0;', 'SELECT CAST(0 AS bigint) AS Server_Id;',  'discord.AddBot must have added a Servers record.');
 	-- Does not create another record if called again.
 	RETURN NEXT lives_ok('SELECT discord.AddBot(0) FOR UPDATE;', 'discord.AddBot must not throw if called again.');
-	RETURN NEXT results_eq('SELECT Server_Id FROM discord.Servers WHERE Server_Id = 0;', 'SELECT bigint 0 AS Server_Id;',  'discord.AddBot must keep existing Servers records.');
+	RETURN NEXT results_eq('SELECT Server_Id FROM discord.Servers WHERE Server_Id = 0;', 'SELECT CAST(0 AS bigint) AS Server_Id;',  'discord.AddBot must keep existing Servers records.');
 END;
 $$ LANGUAGE plpgsql;
